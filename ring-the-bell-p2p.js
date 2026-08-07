36e3b8892452aa753dfadd7315d16440c3d0b145
maktu <maktubhd@gmail.com>
2026-08-07 10:03:52 +0900

    feat: 링더벨 P2P용 결정론적 PRNG/방코드 유틸 모듈 추가

---
+++ ring-the-bell-p2p.js
@@ -0,0 +1,26 @@
+(function (root, factory) {
+  if (typeof module === 'object' && module.exports) module.exports = factory();
+  else root.RingBellP2P = factory();
+})(typeof self !== 'undefined' ? self : this, function () {
+  "use strict";
+
+  function mulberry32(seed) {
+    let a = seed >>> 0;
+    return function () {
+      a |= 0;
+      a = (a + 0x6D2B79F5) | 0;
+      let t = Math.imul(a ^ (a >>> 15), 1 | a);
+      t = (t + Math.imul(t ^ (t >>> 7), 61 | t)) ^ t;
+      return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+    };
+  }
+
+  function makeRoomCode() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let code = '';
+    for (let i = 0; i < 6; i++) code += chars[Math.floor(Math.random() * chars.length)];
+    return code;
+  }
+
+  return { mulberry32, makeRoomCode };
+});
